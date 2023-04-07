6a5f5ecff3cbd2858c984f4c173c96eeb3e4f11f
Chijioke Nwadimkpa <cnwadimkpa@teqbridgeltd.com>
2023-04-07 11:48:25 +0100

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json('hello world !!!');
+  res.json('hello world !!');
 });
 
 const PORT = process.env.PORT || 8000;
