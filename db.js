1d49d3fdbab5fec7a7681931f68a00cf95887b3e
Chijioke Nwadimkpa <cnwadimkpa@teqbridgeltd.com>
2023-04-07 12:36:37 +0100

    ok

--- db.js
+++ db.js
@@ -11,13 +11,14 @@ const { Pool } = require('pg');
 //   // ssl: { rejectUnauthorized: false },
 // });
 
-module.exports = pool;
+// module.exports = pool;
 const pool = new Pool({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
+  ssl: false,
 });
 // const { Pool } = require("pg");
 
@@ -25,4 +26,4 @@ const pool = new Pool({
 //   connectionString: process.env.DATABASE_URL,
 //   ssl: false,
 // });
-// module.exports = pool;
+module.exports = pool;
