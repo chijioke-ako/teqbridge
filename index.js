a41ba93004cf119a1d9991bf460eb4eab3fdde6d
Chijioke Nwadimkpa <cnwadimkpa@teqbridgeltd.com>
2023-04-07 12:27:12 +0100

    database

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
-
+const pool = require('./db');
 const app = express();
+
+app.get('/db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM test');
+    const results = { results: result ? result.rows : null };
+    res.json(results);
+  } catch (err) {
+    console.error(err);
+    res.send('Error ' + err);
+  }
+});
 
 app.get('/', (req, res) => {
   res.json('hello world !!');
